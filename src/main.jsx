70815af075f8d5c9cc93ed81face502db6d47c79
Sushmitha <sushmithakn22@gmail.com>
2026-01-19 16:42:20 +0530

    lost

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/portfolio/">
+  <StrictMode>
     <Toaster />
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
