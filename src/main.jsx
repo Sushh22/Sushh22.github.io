a78dc1987ca1d58f54f96f1c8202e9f99182aca0
Sushmitha <sushmithakn22@gmail.com>
2026-01-19 16:22:55 +0530

    adding browserrouter in main

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
-  <>
+  <BrowserRouter basename="/">
     <Toaster />
     <App />
-  </>
+  </BrowserRouter>
 );
